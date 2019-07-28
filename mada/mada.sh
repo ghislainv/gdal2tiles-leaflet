51454b1394325bd7d770340bc3538669e87b24c2
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2019-07-28 14:18:54 +0200

    Add fcc mada

--- mada/mada.sh
+++ mada/mada.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# conda activate
+# conda activate forestatrisk
+
+#===== Past deforestation
+#gdaldem color-relief data/fcc_recent.tif fcc_color_file.txt data/fcc_RGB.tif -alpha -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
+#gdal2tiles.py -p mercator -r near --processes=3 -s EPSG:32738 -z 0-14 -c Cirad -t "Recent deforestation 2000-2017" -w leaflet data/fcc_RGB.tif tiles_fcc
 
 #===== Probability
+gdaldem color-relief data/prob_icar.tif prob_color_file.txt data/prob_RGB.tif -alpha -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
+gdal2tiles.py -p mercator -r average --processes=3 -s EPSG:32738 -z 0-14 -c Cirad -t "Deforestation probability" -w leaflet data/prob_RGB.tif tiles_prob2010
 
-# Produce a 4 bands raster with RGBA
-# gdaldem color-relief data/prob_icar.tif prob_color_file.txt data/prob_RGB.tif -alpha -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
-
-# With gdal2tiles.py from GDAL
-# gdal2tiles.py -p mercator -r average --processes=3 -s EPSG:32738 -z 0-14 -c Cirad -t "Deforestation probability" -w leaflet data/prob_RGB.tif data/tiles_RGB_14
-
-#===== Forest cover
-gdaldem color-relief data/proj2050_icar.tif proj_color_file.txt data/proj_RGB.tif -alpha -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
-gdal2tiles.py -p mercator -r average --processes=3 -s EPSG:32738 -z 0-14 -c Cirad -t "Forest cover change 2010-2050" -w leaflet data/proj_RGB.tif tiles_proj2050
+#===== Future forest cover
+# gdaldem color-relief data/proj2050_icar.tif proj_color_file.txt data/proj_RGB.tif -alpha -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
+# gdal2tiles.py -p mercator -r near --processes=3 -s EPSG:32738 -z 0-14 -c Cirad -t "Forest cover change 2010-2050" -w leaflet data/proj_RGB.tif tiles_proj2050
