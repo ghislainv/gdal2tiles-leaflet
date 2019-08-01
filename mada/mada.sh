3cc8904e1f8a9497a67b58d7e1437731d8aef088
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2019-08-01 13:52:38 +0300

    Add lines for masoala

--- mada/mada.sh
+++ mada/mada.sh
@@ -3,8 +3,12 @@
 # conda activate forestatrisk
 
 #===== Past deforestation
-gdaldem color-relief data/fcc_recent.tif fcc_color_file.txt data/fcc_RGB.tif -alpha -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
-gdal2tiles.py -p mercator -r near --processes=3 -s EPSG:32738 -z 0-14 -c Cirad -t "Recent deforestation 2000-2017" -w leaflet data/fcc_RGB.tif tiles_fcc
+#gdaldem color-relief data/fcc_recent.tif fcc_color_file.txt data/fcc_RGB.tif -alpha -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
+#gdal2tiles.py -p mercator -r near --processes=3 -s EPSG:32738 -z 0-14 -c Cirad -t "Recent deforestation 2000-2017" -w leaflet data/fcc_RGB.tif tiles_fcc
+
+#===== Past deforestation Masoala
+#gdaldem color-relief data/fcc_recent_masoala.tif fcc_color_file.txt data/fcc_RGB_masoala.tif -alpha -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
+#gdal2tiles.py -p mercator -r near --processes=3 -s EPSG:32738 -z 0-16 -c Cirad -t "Recent deforestation 2000-2017" -w all data/fcc_RGB_masoala.tif tiles_fcc_masoala
 
 #===== Probability
 #gdaldem color-relief data/prob_icar.tif prob_color_file.txt data/prob_RGB.tif -alpha -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
