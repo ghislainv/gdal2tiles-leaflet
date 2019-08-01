e4f0a88887ced118bc6fbe59399cb1d3648bb233
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2019-08-01 13:53:23 +0300

    Add lines for masoala

--- mada/mada.sh
+++ mada/mada.sh
@@ -7,6 +7,7 @@
 #gdal2tiles.py -p mercator -r near --processes=3 -s EPSG:32738 -z 0-14 -c Cirad -t "Recent deforestation 2000-2017" -w leaflet data/fcc_RGB.tif tiles_fcc
 
 #===== Past deforestation Masoala
+#gdal_translate -projwin 919980 8360010 1091970 8220000 -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" data/fcc_recent.tif data/fcc_recent_masoala.tif
 #gdaldem color-relief data/fcc_recent_masoala.tif fcc_color_file.txt data/fcc_RGB_masoala.tif -alpha -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
 #gdal2tiles.py -p mercator -r near --processes=3 -s EPSG:32738 -z 0-16 -c Cirad -t "Recent deforestation 2000-2017" -w all data/fcc_RGB_masoala.tif tiles_fcc_masoala
 
