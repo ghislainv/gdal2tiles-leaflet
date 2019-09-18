c41d2237769f2ad3a9ef4c6659a6565fb69313ea
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2019-09-19 01:55:42 +0200

    Update newcal

--- newcal/newcal.sh
+++ newcal/newcal.sh
@@ -4,6 +4,6 @@
 
 #===== Past deforestation NCL for Locus (mbtiles)
 gdaldem color-relief data/forest_NCL.tif fcc_color_file.txt data/fcc_RGB_newcal.tif -alpha -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9"
-gdalwarp -s_srs EPSG:3163 -t_srs EPSG:3857 -r near -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" data/fcc_RGB_newcal.tif data/fcc_RGB_newcal_mercator.tif 
-gdal_translate -co "RESAMPLING=NEAREST" data/fcc_RGB_newcal_mercator.tif data/fcc_newcal.mbtiles
+gdalwarp -s_srs EPSG:4326 -t_srs EPSG:3857 -r near -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" data/fcc_RGB_newcal.tif data/fcc_RGB_newcal_mercator.tif 
+gdal_translate -co "RESAMPLING=NEAREST" -of MBTiles data/fcc_RGB_newcal_mercator.tif data/fcc_newcal.mbtiles
 gdaladdo -r nearest data/fcc_newcal.mbtiles 2 4 8 16
