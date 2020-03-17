212bc51d7a77eb05ba35dbc32a9e603dea5d89e1
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2020-03-17 13:23:30 +0100

    GDAL COG for continent

--- africa/africa.sh
+++ africa/africa.sh
@@ -2,10 +2,15 @@
 
 # conda activate forestatrisk
 
-gdaladdo -r cubic data/prob_AFR.tif 2 4 8 16
+#=== GDAl COG (Cloud Optimized GeoTIFF)
+gdaladdo -r nearest data-far-tropics/prob.tif 2 4 8 16 32 64 128 256 512 1024 2048
+
+gdal_translate -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "TILED=YES" \
+-co "COPY_SRC_OVERVIEWS=YES" -co "BLOCKXSIZE=256" -co "BLOCKYSIZE=256" \
+data-far-tropics/prob.tif data-far-tropics/prob_tiled.tif
 
 # Geoserver, Building and using an image pyramid
 # https://docs.geoserver.org/stable/en/user/tutorials/imagepyramid/imagepyramid.html
-
-#===== Probability
-gdal_retile.py -useDirForEachRow -r bilinear -levels 4 -ps 2048 2048 -co "TILED=YES" -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" -co "BIGTIFF=YES" -targetDir pyramid_prob_AFR data/prob.tif
+gdal_retile.py -v -useDirForEachRow -r near -levels 7 -ps 2048 2048 \
+-co "TILED=YES" -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BLOCKXSIZE=256" -co "BLOCKYSIZE=256" \
+-targetDir pyramid_prob_AFR data/prob.tif
--- mada/mada.sh
+++ mada/mada.sh
@@ -14,15 +14,22 @@
 # gdaldem color-relief data/proj2050_icar.tif proj_color_file.txt data/proj_RGB.tif -alpha -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
 # gdal2tiles.py -p mercator -r near --processes=3 -s EPSG:32738 -z 0-14 -c Cirad -t "Forest cover change 2010-2050" -w leaflet data/proj_RGB.tif tiles_proj2050
 
-#===== Past deforestation Masoala for Locus (mbtiles)
-gdal_translate -projwin 919980 8360010 1091970 8220000 -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" data/fcc_recent.tif data/fcc_recent_masoala.tif
-gdaldem color-relief data/fcc_recent_masoala.tif fcc_color_file.txt data/fcc_RGB_masoala.tif -alpha -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9"
-gdalwarp -s_srs EPSG:32738 -t_srs EPSG:3857 -r near -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" data/fcc_RGB_masoala.tif data/fcc_RGB_masoala_mercator.tif 
-gdal_translate -co "RESAMPLING=NEAREST" -of MBTiles data/fcc_RGB_masoala_mercator.tif data/fcc_masoala_bioscene.mbtiles
-gdaladdo -r nearest data/fcc_masoala_bioscene.mbtiles 2 4 8 16
+# #===== Past deforestation Masoala for Locus (mbtiles)
+# gdal_translate -projwin 919980 8360010 1091970 8220000 -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" data/fcc_recent.tif data/fcc_recent_masoala.tif
+# gdaldem color-relief data/fcc_recent_masoala.tif fcc_color_file.txt data/fcc_RGB_masoala.tif -alpha -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9"
+# gdalwarp -s_srs EPSG:32738 -t_srs EPSG:3857 -r near -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" data/fcc_RGB_masoala.tif data/fcc_RGB_masoala_mercator.tif 
+# gdal_translate -co "RESAMPLING=NEAREST" -of MBTiles data/fcc_RGB_masoala_mercator.tif data/fcc_masoala_bioscene.mbtiles
+# gdaladdo -r nearest data/fcc_masoala_bioscene.mbtiles 2 4 8 16
 
 #===== Past deforestation Masoala Christelle for Locus (mbtiles)
 #gdaldem color-relief data/fcc_masoala_christelle.tif fcc_color_file.txt data/fcc_RGB_masoala_christelle.tif -alpha -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9"
 #gdalwarp -s_srs EPSG:4326 -t_srs EPSG:3857 -r near -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" data/fcc_RGB_masoala_christelle.tif data/fcc_RGB_masoala_christelle_mercator.tif 
 #gdal_translate -co "RESAMPLING=NEAREST" -of MBTiles data/fcc_RGB_masoala_christelle_mercator.tif data/fcc_masoala_christelle.mbtiles
 #gdaladdo -r nearest data/fcc_masoala_christelle.mbtiles 2 4 8 16
+
+#===== Probability geoserver
+gdal_retile.py -v -useDirForEachRow -r near -levels 4 -ps 2048 2048 -co "TILED=YES" -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BLOCKXSIZE=256" -co "BLOCKYSIZE=256" -targetDir pyramid_prob_MDG data-far-tropics/prob.tif
+
+#=== GDAl COG (Cloud Optimized GeoTIFF)
+gdaladdo -r nearest data-far-tropics/prob.tif 2 4 8 16 32 64 128 256 512 1024 2048
+gdal_translate -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "TILED=YES" -co "COPY_SRC_OVERVIEWS=YES" -co "BLOCKXSIZE=256" -co "BLOCKYSIZE=256" data-far-tropics/prob.tif data-far-tropics/prob_tiled.tif
